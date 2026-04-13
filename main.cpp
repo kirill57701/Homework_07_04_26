e99f8ce27dc8a140d696c0033002ee83b40c9e3d
kirill <kirillpetrov56333@gmail.com>
2026-04-13 19:56:35 +0300

    test hasnext

--- main.cpp
+++ main.cpp
@@ -3,21 +3,48 @@
 
 template<class T, size_t K>
 struct BTree {
-  T val[K];
-  BTree<T, K>*	childs[K + 1];
-  BTree<T, K>*	parent;
+	T val[K];
+	BTree<T, K>*	childs[K + 1];
+	BTree<T, K>*	parent;
 };
 
 template<class T, size_t K>
 struct BTreeIt {
-  size_t s;
-  BTree<T, K>* current;
+	size_t s;
+	BTree<T, K>* current;
 };
 
 template<class T, size_t K>
 T value(BTreeIt<T, K> it) {
-  if (it.current == nullptr || it.s >= K) {
-    throw std::runtime_error();
-  }
-  return it.current->val[it.s];
+	if (it.current == nullptr || it.s > K) {
+		throw std::logic_error();
+	}
+	return it.current->val[it.s];
 }
+
+template<class T, size_t K>
+bool hasNext(BTreeIt<T, K> it) {
+	if (it.current == nullptr || s > K) {
+		throw std::logic_error();
+	}
+	if (it.s < K) {
+		return 1;
+	}
+	for (size_t i = 0; i < K +1; ++i) {
+		if (it.current->childs[i] != nullptr) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+/*template<class T, size_t K>
+BTreeIt<T, K> next(BTreeIt<T, K> it) {
+	if (it.current == nullptr || it.s > K) {
+		throw std::logic_error();
+	}
+	if (it.s < K) {
+		return it.current->val[++it.s];
+	}
+
+}*/
