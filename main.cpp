c6c8f9ee6a2124b6585120f6ac28da9767ce70e1
kirill <kirillpetrov56333@gmail.com>
2026-04-13 19:41:51 +0300

    fixx

--- main.cpp
+++ main.cpp
@@ -1,17 +1,23 @@
+#include <cstddef>
+#include <stdexcept>
+
 template<class T, size_t K>
 struct BTree {
-	T val[K];
-	BTree<T, K>*	childs[K + 1];
-	BTree<T, K>*	parent;
+  T val[K];
+  BTree<T, K>*	childs[K + 1];
+  BTree<T, K>*	parent;
 };
 
 template<class T, size_t K>
 struct BTreeIt {
-	size_t s;
-	BTree<T, K>* current;
+  size_t s;
+  BTree<T, K>* current;
 };
 
 template<class T, size_t K>
-T value(BTree<T, K> it) {
-
+T value(BTreeIt<T, K> it) {
+  if (it.current == nullptr || it.s >= K) {
+    throw std::runtime_error();
+  }
+  return it.current->val[it.s];
 }
