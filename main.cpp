b1d48933077cb9d56fe0ed49eada3a39d497c89e
Kirill Petrov <kirillpetrov56333@gmail.com>
2026-05-01 10:11:07 +0300

    adding homework

--- main.cpp
+++ main.cpp
@@ -335,3 +335,34 @@ InclusionI<T> next(InclusionIt<T> curr, BiTree< T >* pattern);
 
 template< class T >
 bool hasNext(InclusionIt<T> curr, BiTree< T >* pattern);
+
+template< class T > struct InclusionIt {
+  std::pair< BiTree<T>*, BiTree<T>* > incl;
+};
+
+template< class T >
+InclusionIt<T> begin(BiTree<T>* lhs, BiTree<T>* pattern)
+{
+  BiTree< T >* beg = fallLeft(lhs).second;
+  InclusionIt<T> res = inclusionStart(beg, pattern);
+  return res;
+}
+
+template< class T >
+InclusionIt<T> next(InclusionIt<T> curr, BiTree< T >* pattern)
+{
+  if (!curr.incl.first) {
+    auto res = make_pair(nullptr, nullptr);
+    return res;
+  }
+
+  BiTree< T >* nexs = nextStruct(curr.incl.first).second;
+  InclusionIt<T> res = inclusionStart(nexs, pattern);
+  return res;
+}
+
+template< class T >
+bool hasNext(InclusionIt<T> curr, BiTree< T >* pattern)
+{
+  return curr.incl.first != nullptr;
+}
